5a24919c0e96d2bf37f8e88714116f6392295f0f
yunqi <yunqi.zhang@foxmail.com>
2023-09-24 15:44:17 +0800

    verifyCode 简单实现

--- verifyCode/internal/service/verifycode.go
+++ verifyCode/internal/service/verifycode.go
@@ -2,6 +2,9 @@ package service
 
 import (
 	"context"
+	"math/rand"
+	"strings"
+	"time"
 
 	pb "verifyCode/api/verifyCode"
 )
@@ -24,8 +27,39 @@ func (s *VerifyCodeService) DeleteVerifyCode(ctx context.Context, req *pb.Delete
 	return &pb.DeleteVerifyCodeReply{}, nil
 }
 func (s *VerifyCodeService) GetVerifyCode(ctx context.Context, req *pb.GetVerifyCodeRequest) (*pb.GetVerifyCodeReply, error) {
-	return &pb.GetVerifyCodeReply{}, nil
+	return &pb.GetVerifyCodeReply{
+		Code: randCode(codeChars(req.Type), int(req.Length)),
+	}, nil
 }
 func (s *VerifyCodeService) ListVerifyCode(ctx context.Context, req *pb.ListVerifyCodeRequest) (*pb.ListVerifyCodeReply, error) {
 	return &pb.ListVerifyCodeReply{}, nil
+}
+
+func codeChars(t pb.TYPE) string {
+	var chars string
+	switch t {
+	case pb.TYPE_Default:
+	case pb.TYPE_DIGIT:
+		chars = "0123456789"
+	case pb.TYPE_LETTER:
+		chars = "abcdefghijklmnopqrstuvwxyz"
+	case pb.TYPE_MIXED:
+		chars = "0123456789abcdefghijklmnopqrstuvwxyz"
+	}
+	return chars
+}
+func randCode(chars string, l int) string {
+
+	// 利用string builder构建结果缓冲
+	sb := strings.Builder{}
+	sb.Grow(l)
+	charsLen := len(chars)
+
+	rn := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < l; i++ {
+		randIndex := rn.Intn(charsLen)
+		sb.WriteByte(chars[randIndex])
+	}
+
+	return sb.String()
 }
