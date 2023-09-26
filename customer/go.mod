026f834d20bfabbc5d667497d117d36321fb64c9
yunqi <yunqi.zhang@foxmail.com>
2023-09-26 10:53:15 +0800

    customer add mysql

--- customer/go.mod
+++ customer/go.mod
@@ -20,10 +20,13 @@ require (
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-playground/form/v4 v4.2.0 // indirect
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	go.opentelemetry.io/otel v1.7.0 // indirect
 	go.opentelemetry.io/otel/trace v1.7.0 // indirect
@@ -32,4 +35,6 @@ require (
 	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/driver/mysql v1.5.1 // indirect
+	gorm.io/gorm v1.25.4 // indirect
 )
--- customer/internal/data/data.go
+++ customer/internal/data/data.go
@@ -5,6 +5,8 @@ import (
 	"customer/internal/conf"
 	"fmt"
 	"github.com/redis/go-redis/v9"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/google/wire"
@@ -17,6 +19,7 @@ var ProviderSet = wire.NewSet(NewData, NewGreeterRepo, NewCustomerData)
 type Data struct {
 	// TODO wrapped database client
 	redis *redis.Client
+	mysql *gorm.DB
 }
 
 func (d *Data) Redis() *redis.Client {
@@ -27,6 +30,7 @@ func (d *Data) Redis() *redis.Client {
 func NewData(c *conf.Data, logger log.Logger) (*Data, func(), error) {
 	data := &Data{
 		redis: initRedis(c),
+		mysql: initMysql(c),
 	}
 	cleanup := func() {
 		log.NewHelper(logger).Info("closing the data resources")
@@ -45,4 +49,13 @@ func initRedis(c *conf.Data) *redis.Client {
 		panic(status.Err())
 	}
 	return client
+}
+
+func initMysql(c *conf.Data) *gorm.DB {
+	//dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(c.Database.Source), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	return db
 }
