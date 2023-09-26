011e3c4ca057b9aa3848e82b23781527524bb5c6
yunqi <yunqi.zhang@foxmail.com>
2023-09-26 20:42:32 +0800

    customer login jwt middleware

--- customer/internal/server/http.go
+++ customer/internal/server/http.go
@@ -1,14 +1,31 @@
 package server
 
 import (
+	"context"
 	"customer/api/customer"
 	v1 "customer/api/helloworld/v1"
 	"customer/internal/conf"
 	"customer/internal/service"
 	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware/auth/jwt"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/middleware/selector"
 	"github.com/go-kratos/kratos/v2/transport/http"
+	jwt2 "github.com/golang-jwt/jwt/v4"
 )
+
+func NewWhiteListMatcher() selector.MatchFunc {
+
+	whiteList := make(map[string]struct{})
+	whiteList["/shop.interface.v1.ShopInterface/Login"] = struct{}{}
+	whiteList["/shop.interface.v1.ShopInterface/Register"] = struct{}{}
+	return func(ctx context.Context, operation string) bool {
+		if _, ok := whiteList[operation]; ok {
+			return false
+		}
+		return true
+	}
+}
 
 // NewHTTPServer new an HTTP server.
 func NewHTTPServer(c *conf.Server, greeter *service.GreeterService,
@@ -17,6 +34,15 @@ func NewHTTPServer(c *conf.Server, greeter *service.GreeterService,
 	var opts = []http.ServerOption{
 		http.Middleware(
 			recovery.Recovery(),
+			selector.Server(
+				jwt.Server(func(token *jwt2.Token) (interface{}, error) {
+					return []byte("aaaa"), nil
+				}, jwt.WithSigningMethod(jwt2.SigningMethodHS256), jwt.WithClaims(func() jwt2.Claims {
+					return &jwt2.MapClaims{}
+				})),
+			).
+				Match(NewWhiteListMatcher()).
+				Build(),
 		),
 	}
 	if c.Http.Network != "" {
