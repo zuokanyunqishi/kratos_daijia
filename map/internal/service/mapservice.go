d4f9ca9658d06e150d3217c03f59331cfb7dc5dc
yunqi <yunqi.zhang@foxmail.com>
2023-09-28 22:13:24 +0800

    map service

---
+++ map/internal/service/mapservice.go
@@ -0,0 +1,19 @@
+package service
+
+import (
+	"context"
+
+	pb "map/api/mapService"
+)
+
+type MapServiceService struct {
+	pb.UnimplementedMapServiceServer
+}
+
+func NewMapServiceService() *MapServiceService {
+	return &MapServiceService{}
+}
+
+func (s *MapServiceService) GetDrivingInfo(ctx context.Context, req *pb.GetDrivingInfoReq) (*pb.GetDrivingInfoResp, error) {
+	return &pb.GetDrivingInfoResp{}, nil
+}
