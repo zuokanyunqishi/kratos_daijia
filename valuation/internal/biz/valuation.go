13b763758a75d44c520d4e4a9ef39947f859a227
yunqi <yunqi.zhang@foxmail.com>
2023-11-11 10:05:34 +0800

    valuation config.yaml

--- valuation/internal/biz/valuation.go
+++ valuation/internal/biz/valuation.go
@@ -1,1 +1,20 @@
 package biz
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type PrizeRule struct {
+	gorm.Model
+	PrizeRuleWork
+}
+
+type PrizeRuleWork struct {
+	CityID      uint      `json:"city_id" gorm:"type:int;not null"`
+	StartFree   int64     `json:"start_free" gorm:"type:int;not null"`
+	DistanceFee int64     `json:"distance_free" gorm:"type:int;not null"`
+	DurationFee int64     `json:"duration_fee" gorm:"type:int;not null"`
+	StartAt     time.Time `json:"start_at" gorm:"type:time;not null"`
+	EndAt       time.Time `json:"end_at" gorm:"type:time;not null"`
+}
--- valuation/internal/data/data.go
+++ valuation/internal/data/data.go
@@ -1,6 +1,13 @@
 package data
 
 import (
+	"context"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"valuation/internal/biz"
 	"valuation/internal/conf"
 
 	"github.com/go-kratos/kratos/v2/log"
@@ -10,15 +17,55 @@ import (
 // ProviderSet is data providers.
 var ProviderSet = wire.NewSet(NewData, NewGreeterRepo)
 
-// Data .
 type Data struct {
-	// TODO wrapped database client
+	redis *redis.Client
+	mysql *gorm.DB
+}
+
+func (d *Data) Redis() *redis.Client {
+	return d.redis
 }
 
 // NewData .
 func NewData(c *conf.Data, logger log.Logger) (*Data, func(), error) {
+	data := &Data{
+		redis: initRedis(c),
+		mysql: initMysql(c),
+	}
 	cleanup := func() {
 		log.NewHelper(logger).Info("closing the data resources")
+		// 关掉 redis 连接
+		data.redis.Close()
 	}
-	return &Data{}, cleanup, nil
+	// 初始化表模型
+	migrateTable(data.mysql)
+	return data, cleanup, nil
+}
+
+func initRedis(c *conf.Data) *redis.Client {
+	url := fmt.Sprintf("redis://%s/1?dial_timeout=%d", c.Redis.GetAddr(), 1)
+	options, _ := redis.ParseURL(url)
+	client := redis.NewClient(options)
+	status := client.Ping(context.Background())
+	if status.Err() != nil {
+		panic(status.Err())
+	}
+	return client
+}
+
+func initMysql(c *conf.Data) *gorm.DB {
+	//dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(c.Database.Source), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Error),
+	})
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
+func migrateTable(db *gorm.DB) {
+	if err := db.AutoMigrate(&biz.PrizeRule{}); err != nil {
+		log.Error(err)
+	}
 }
