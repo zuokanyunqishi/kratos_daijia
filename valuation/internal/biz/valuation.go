86e3a3d7636f0eebb060df717bb0873f07a1d7e2
yunqi <yunqi.zhang@foxmail.com>
2023-11-23 09:51:39 +0800

    valuation prizeRule

--- valuation/internal/biz/valuation.go
+++ valuation/internal/biz/valuation.go
@@ -1,8 +1,9 @@
 package biz
 
 import (
+	"context"
+	"github.com/go-kratos/kratos/v2/log"
 	"gorm.io/gorm"
-	"time"
 )
 
 type PrizeRule struct {
@@ -11,10 +12,28 @@ type PrizeRule struct {
 }
 
 type PrizeRuleWork struct {
-	CityID      uint      `json:"city_id" gorm:"type:int;not null"`
-	StartFree   int64     `json:"start_free" gorm:"type:int;not null"`
-	DistanceFee int64     `json:"distance_free" gorm:"type:int;not null"`
-	DurationFee int64     `json:"duration_fee" gorm:"type:int;not null"`
-	StartAt     time.Time `json:"start_at" gorm:"type:time;not null"`
-	EndAt       time.Time `json:"end_at" gorm:"type:time;not null"`
+	CityID      uint  `json:"city_id" gorm:"type:int;not null"`
+	StartFree   int64 `json:"start_free" gorm:"type:int;not null"`
+	DistanceFee int64 `json:"distance_free" gorm:"type:int;not null"`
+	DurationFee int64 `json:"duration_fee" gorm:"type:int;not null"`
+	StartAt     int   `json:"start_at" gorm:"type:int;not null"`
+	EndAt       int   `json:"end_at" gorm:"type:int;not null"`
+}
+
+type PrizeRuleRepo interface {
+	GetRule(ctx context.Context, cityId uint, curr int) (*PrizeRule, error)
+}
+
+type ValuationBiz struct {
+	log *log.Helper
+	pri PrizeRuleRepo
+}
+
+func (b *ValuationBiz) GetRuleInfo(ctx context.Context, cityId uint, curr int) (*PrizeRule, error) {
+
+	return b.pri.GetRule(ctx, cityId, curr)
+}
+
+func NewValuationBiz(pri PrizeRuleRepo, logger log.Logger) *ValuationBiz {
+	return &ValuationBiz{log: log.NewHelper(logger), pri: pri}
 }
--- valuation/internal/data/data.go
+++ valuation/internal/data/data.go
@@ -15,7 +15,7 @@ import (
 )
 
 // ProviderSet is data providers.
-var ProviderSet = wire.NewSet(NewData, NewGreeterRepo)
+var ProviderSet = wire.NewSet(NewData, NewGreeterRepo, NewPrizeRuleData)
 
 type Data struct {
 	redis *redis.Client
