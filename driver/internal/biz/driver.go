e3637423a1ca7b19ed5c1971f15fccd481f0051e
yunqi <yunqi.zhang@foxmail.com>
2023-12-09 19:59:14 +0800

    driver init.sql

--- driver/internal/biz/driver.go
+++ driver/internal/biz/driver.go
@@ -21,17 +21,17 @@ type Driver struct {
 
 // DriverWork 司机的业务模型
 type DriverWork struct {
-	Telephone     string       `gorm:"column:telephone;type:varchar(16);" json:"telephone"`
-	Token         string       `gorm:"column:token;type:varchar(2047);" json:"token"`
-	Name          string       `gorm:"column:name;type:varchar(255);" json:"name"`
-	Status        string       `gorm:"column:status;type:enum('out','in','listen','stop');" json:"status"`
-	IdNumber      string       `gorm:"column:id_number;type:varchar(18);" json:"id_number"`
-	IdImageA      string       `gorm:"column:id_image_a;type:varchar(255);" json:"id_image_a"`
-	LicenceImageA string       `gorm:"column:licence_image_a;type:varchar(255);" json:"licence_image_a"`
-	LicenceImageB string       `gorm:"column:licence_image_b;type:varchar(255);" json:"licence_image_b"`
-	DistinctCode  string       `gorm:"column:distinct_code;type:varchar(16);" json:"distinct_code"`
-	AuditAt       sql.NullTime `gorm:"column:audit_at;type:datetime;" json:"audit_at"`
-	TelephoneBak  string       `gorm:"column:telephone_bak;type:varchar(16);" json:"telephone_bak"`
+	Telephone     string         `gorm:"column:telephone;type:varchar(16);uniqueIndex;" json:"telephone"`
+	Token         sql.NullString `gorm:"column:token;type:varchar(2047);" json:"token"`
+	Name          sql.NullString `gorm:"column:name;type:varchar(255);index;" json:"name"`
+	Status        sql.NullString `gorm:"column:status;type:enum('out','in','listen','stop');" json:"status"`
+	IdNumber      sql.NullString `gorm:"column:id_number;type:varchar(18);uniqueIndex;" json:"id_number"`
+	IdImageA      sql.NullString `gorm:"column:id_image_a;type:varchar(255);" json:"id_image_a"`
+	LicenceImageA sql.NullString `gorm:"column:licence_image_a;type:varchar(255);" json:"licence_image_a"`
+	LicenceImageB sql.NullString `gorm:"column:licence_image_b;type:varchar(255);" json:"licence_image_b"`
+	DistinctCode  sql.NullString `gorm:"column:distinct_code;type:varchar(16);index;" json:"distinct_code"`
+	AuditAt       sql.NullTime   `gorm:"column:audit_at;type:datetime;index;" json:"audit_at"`
+	TelephoneBak  sql.NullString `gorm:"column:telephone_bak;type:varchar(16);" json:"telephone_bak"`
 }
 
 type VerifyCodeRepo interface {
